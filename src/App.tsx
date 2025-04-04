7adc550f678dfa48d807354642c5f7bd43af68c7
Muhammad <muhammad.safarov@web.de>
2025-04-04 12:06:24 +0200

    Created basic structure of the project, Footer, started developing Navbar

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Layout from "./components/Layout";
+import Hero from "./components/sections/Hero";
+
+function App() {
+  return <Layout title="NexAI">
+      <Hero />
+  </Layout>;
+}
+
+export default App;
